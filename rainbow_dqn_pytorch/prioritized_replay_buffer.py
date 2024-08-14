b0197c0cc034d3cd8162694332d8ec96567be53f
Noah Rothgaber <noahrothgaber@gmail.com>
2024-08-14 16:25:06 -0400

    Added newest version of regular replay memory class, to go along with prioritized exeperience replay buffer.

--- rainbow_dqn_pytorch/prioritized_replay_buffer.py
+++ rainbow_dqn_pytorch/prioritized_replay_buffer.py
@@ -5,7 +5,14 @@ from typing import Dict, List, Tuple
 class ReplayBuffer:
     """A simple numpy replay buffer."""
 
-    def __init__(self, obs_dim: int, size: int, batch_size: int = 32):
+    def __init__(
+        self, 
+        obs_dim: int, 
+        size: int, 
+        batch_size: int = 32, 
+        n_step: int = 1, 
+        gamma: float = 0.99
+    ):
         self.obs_buf = np.zeros([size, obs_dim], dtype=np.float32)
         self.next_obs_buf = np.zeros([size, obs_dim], dtype=np.float32)
         self.acts_buf = np.zeros([size], dtype=np.float32)
@@ -13,15 +20,33 @@ class ReplayBuffer:
         self.done_buf = np.zeros(size, dtype=np.float32)
         self.max_size, self.batch_size = size, batch_size
         self.ptr, self.size, = 0, 0
+        
+        # for N-step Learning
+        self.n_step_buffer = deque(maxlen=n_step)
+        self.n_step = n_step
+        self.gamma = gamma
 
     def store(
-        self,
-        obs: np.ndarray,
+        self, 
+        obs: np.ndarray, 
         act: np.ndarray, 
         rew: float, 
         next_obs: np.ndarray, 
         done: bool,
-    ):
+    ) -> Tuple[np.ndarray, np.ndarray, float, np.ndarray, bool]:
+        transition = (obs, act, rew, next_obs, done)
+        self.n_step_buffer.append(transition)
+
+        # single step transition is not ready
+        if len(self.n_step_buffer) < self.n_step:
+            return ()
+        
+        # make a n-step transition
+        rew, next_obs, done = self._get_n_step_info(
+            self.n_step_buffer, self.gamma
+        )
+        obs, act = self.n_step_buffer[0][:2]
+        
         self.obs_buf[self.ptr] = obs
         self.next_obs_buf[self.ptr] = next_obs
         self.acts_buf[self.ptr] = act
@@ -29,14 +54,48 @@ class ReplayBuffer:
         self.done_buf[self.ptr] = done
         self.ptr = (self.ptr + 1) % self.max_size
         self.size = min(self.size + 1, self.max_size)
+        
+        return self.n_step_buffer[0]
 
     def sample_batch(self) -> Dict[str, np.ndarray]:
         idxs = np.random.choice(self.size, size=self.batch_size, replace=False)
-        return dict(obs=self.obs_buf[idxs],
-                    next_obs=self.next_obs_buf[idxs],
-                    acts=self.acts_buf[idxs],
-                    rews=self.rews_buf[idxs],
-                    done=self.done_buf[idxs])
+
+        return dict(
+            obs=self.obs_buf[idxs],
+            next_obs=self.next_obs_buf[idxs],
+            acts=self.acts_buf[idxs],
+            rews=self.rews_buf[idxs],
+            done=self.done_buf[idxs],
+            # for N-step Learning
+            indices=idxs,
+        )
+    
+    def sample_batch_from_idxs(
+        self, idxs: np.ndarray
+    ) -> Dict[str, np.ndarray]:
+        # for N-step Learning
+        return dict(
+            obs=self.obs_buf[idxs],
+            next_obs=self.next_obs_buf[idxs],
+            acts=self.acts_buf[idxs],
+            rews=self.rews_buf[idxs],
+            done=self.done_buf[idxs],
+        )
+    
+    def _get_n_step_info(
+        self, n_step_buffer: Deque, gamma: float
+    ) -> Tuple[np.int64, np.ndarray, bool]:
+        """Return n step rew, next_obs, and done."""
+        # info of the last transition
+        rew, next_obs, done = n_step_buffer[-1][-3:]
+
+        for transition in reversed(list(n_step_buffer)[:-1]):
+            r, n_o, d = transition[-3:]
+
+            rew = r + gamma * rew * (1 - d)
+            next_obs, done = (n_o, d) if d else (next_obs, done)
+
+        return rew, next_obs, done
 
     def __len__(self) -> int:
         return self.size
@@ -55,15 +114,19 @@ class PrioritizedReplayBuffer(ReplayBuffer):
     
     def __init__(
         self, 
-        obs_dim: int,
+        obs_dim: int, 
         size: int, 
         batch_size: int = 32, 
-        alpha: float = 0.6
+        alpha: float = 0.6,
+        n_step: int = 1, 
+        gamma: float = 0.99,
     ):
         """Initialization."""
         assert alpha >= 0
         
-        super(PrioritizedReplayBuffer, self).__init__(obs_dim, size, batch_size)
+        super(PrioritizedReplayBuffer, self).__init__(
+            obs_dim, size, batch_size, n_step, gamma
+        )
         self.max_priority, self.tree_ptr = 1.0, 0
         self.alpha = alpha
         
@@ -81,14 +144,17 @@ class PrioritizedReplayBuffer(ReplayBuffer):
         act: int, 
         rew: float, 
         next_obs: np.ndarray, 
-        done: bool
-    ):
+        done: bool,
+    ) -> Tuple[np.ndarray, np.ndarray, float, np.ndarray, bool]:
         """Store experience and priority."""
-        super().store(obs, act, rew, next_obs, done)
+        transition = super().store(obs, act, rew, next_obs, done)
         
-        self.sum_tree[self.tree_ptr] = self.max_priority ** self.alpha
-        self.min_tree[self.tree_ptr] = self.max_priority ** self.alpha
-        self.tree_ptr = (self.tree_ptr + 1) % self.max_size
+        if transition:
+            self.sum_tree[self.tree_ptr] = self.max_priority ** self.alpha
+            self.min_tree[self.tree_ptr] = self.max_priority ** self.alpha
+            self.tree_ptr = (self.tree_ptr + 1) % self.max_size
+        
+        return transition
 
     def sample_batch(self, beta: float = 0.4) -> Dict[str, np.ndarray]:
         """Sample a batch of experiences."""
